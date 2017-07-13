38a714c84117b373e75d655e6e73825bcf5aedb9
Shuistlo <aredpenguin1@yahoo.com>
2017-07-13 18:09:26 +0100

    work in progress

---
+++ RosalindChallenges/src/Enumerating_Oriented_Gene_Orderings.java
@@ -0,0 +1,17 @@
+
+public class Enumerating_Oriented_Gene_Orderings {
+	
+	public int[] generateChoice(int end){
+		int[] cList = new int[end*2];
+		for(int i = 1; i == end; i++){ //wont work
+			cList[i] = i;
+		}
+		
+		return cList;
+	}
+
+	public Enumerating_Oriented_Gene_Orderings() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
